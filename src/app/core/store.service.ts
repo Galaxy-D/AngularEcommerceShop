0cdc2949eef2e42b0fe16c88049bdb372fa9efe8
Amine BENLAMA <benlama.amine@gmail.com>
2023-09-25 21:27:37 +0200

    create a store service that fetch data from fake api

--- src/app/core/store.service.ts
+++ src/app/core/store.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../shared/model/product.model';
+import { Observable } from 'rxjs/internal/Observable';
+
+const STORE_BASE_URL = 'https://fakestoreapi.com';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getAllProducts(
+    limit = '10',
+    sort = 'desc',
+    category?: string
+  ): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(
+      `${STORE_BASE_URL}/products${category ? '/category/' + category : ''}?sort=${sort}&limit=${limit}`
+    );
+  }
+
+  getAllCategories(): Observable<string[]> {
+    return this.httpClient.get<string[]>(`${STORE_BASE_URL}/products/categories`);
+  }
 }
